70ab2cdec71e641d694226145f02e1c430fd95c9
Ankur <ankurshukla1360@gmail.com>
2026-06-19 05:22:48 +0530

    fix auth files

--- models/user.js
+++ models/user.js
@@ -1,23 +1,13 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
     },
 });
 
-// 👇 SMART FIX: Node.js v22 Import Error Fix
-if (typeof passportLocalMongoose === 'function') {
-    // Normal import
-    userSchema.plugin(passportLocalMongoose);
-} else if (passportLocalMongoose.default) {
-    // Nested default import (For newer Node versions)
-    userSchema.plugin(passportLocalMongoose.default);
-} else {
-    console.error("🚨 Error loading passport-local-mongoose");
-}
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
-const passport = require('passport');
+const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", async (req, res, next) => { // 'next' add kiya
+router.post("/signup", async (req, res, next) => {
     try {
-        let { username, email, password } = req.body;
-        const newUser = new User({ email, username });
+        const { username, email, password } = req.body;
+
+        const newUser = new User({
+            username,
+            email,
+        });
+
         const registeredUser = await User.register(newUser, password);
-        
+
         req.login(registeredUser, (err) => {
-            if (err) return next(err);
-            req.flash("success", "Welcome to Wanderlust! Your account is created.");
+            if (err) {
+                return next(err);
+            }
+
+            req.flash(
+                "success",
+                "Welcome to Wanderlust! Your account is created."
+            );
+
             res.redirect("/listings");
         });
-    } catch (e) {
-        req.flash("error", e.message);
+    } catch (err) {
+        console.log("SIGNUP ERROR =>", err);
+
+        req.flash("error", err.message);
         res.redirect("/signup");
     }
 });
@@ -28,20 +42,28 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login", 
+router.post(
+    "/login",
     passport.authenticate("local", {
         failureRedirect: "/login",
         failureFlash: true,
-    }), 
-    async (req, res) => {
-        req.flash("success", "Welcome back to Wanderlust! You are logged in.");
+    }),
+    (req, res) => {
+        req.flash(
+            "success",
+            "Welcome back to Wanderlust! You are logged in."
+        );
+
         res.redirect("/listings");
     }
 );
 
 router.get("/logout", (req, res, next) => {
     req.logout((err) => {
-        if (err) return next(err);
+        if (err) {
+            return next(err);
+        }
+
         req.flash("success", "You are logged out now!");
         res.redirect("/listings");
     });
